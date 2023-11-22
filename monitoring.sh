127dc54dfa086f8069c246a9f7929dc60a00d336
João Pedro Bergfeld Backer Lopes <113688538+jpedrobacker@users.noreply.github.com>
2023-11-22 16:15:11 -0300

    Create monitoring.sh

---
+++ monitoring.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+The number of virtual processors: uname -a
+The number of physical processors: grep "physical id" /proc/cpuinfo | sort | uniq | wc -l
+The number of virtual processors: 
+The number of virtual processors: 
+The current available RAM on your server and its utilization rate as a percentage: 
+The current available memory on your server and its utilization rate as a percentage: 
+The current utilization rate of your processors as a percentage: 
+The date and time of the last reboot: 
+Whether LVM is active or not: 
+The number of active connections: 
+The number of users using the server: 
+The IPv4 address of your server and its MAC (Media Access Control) address: 
+The number of commands executed with the sudo program: 
