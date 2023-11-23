9811cb1503422efdafc9723a1ceeb7480f29fafe
jpedrob <jpedrobacker@outlook.com>
2023-11-23 15:57:52 -0300

    added more to the script

--- monitoring.sh
+++ monitoring.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-The architecture of your operating system and its kernel version: uname -a
-The number of physical processors: grep "physical id" /proc/cpuinfo | sort | uniq | wc -l
-The number of virtual processors: 
-The current available RAM on your server and its utilization rate as a percentage: 
-The current available memory on your server and its utilization rate as a percentage: 
-The current utilization rate of your processors as a percentage: 
-The date and time of the last reboot: 
-Whether LVM is active or not: 
-The number of active connections: 
-The number of users using the server: 
-The IPv4 address of your server and its MAC (Media Access Control) address: 
-The number of commands executed with the sudo program: 
+arch=$(uname -a)
+cpup=$(cat /proc/cpuinfo | grep "physical id" | wc -l)
+cpuv=$(cat /proc/cpuinfo | grep processor | wc -l)
+mem=$(free -m | grep Mem | awk '{printf("%.1f/%.fGb (%s)", $3, $2, ($3/$2)/100)}')
+disku=$(df -h --total | grep total | awk '{printf("%.1f/%fGb (%s)", $3, $2, $5)}')
+The current utilization rate of your processors as a percentage:
+The date and time of the last reboot:
+Whether LVM is active or not:
+The number of active connections:
+The number of users using the server:
+The IPv4 address of your server and its MAC (Media Access Control) address:
+The number of commands executed with the sudo program:
